d503dd5e8906db5f8fd4ad3d3d6c30167f4fa49a
Ashok Lamsal <ashoklamsal138@gmail.com>
2025-08-06 07:39:09 +0545

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/portfolio-website/',
+  base: 'homepage": "https://ashok053.github.io/portfolio-website',
   plugins: [react()],
   resolve: {
     alias: {
